220c8a17c06b90ab006bf17639f6ad62bad4cf0a
ananta111 <ananta.thapaliya01@gmail.com>
2017-06-21 11:05:32 -0600

    Update helloworld.cpp

--- plugins/helloworld.cpp
+++ plugins/helloworld.cpp
@@ -78,57 +78,7 @@ bool HelloWorld::run()
     pluginProgress->progress(0, STEPS);
   }
 
-  /**
-   * while this does not import
-   * nodes/edges, it imports properties
-   * for an existing fabric
-   */
-
-  /*ib::tulip_fabric_t * const fabric = ib::tulip_fabric_t::find_fabric(graph, false);
-  if(!fabric)
-  {
-    if(pluginProgress)
-      pluginProgress->setError("Unable find fabric. Make sure to preserve data when importing data.");
-
-    return false;
-  }
-
-  if(pluginProgress)
-  {
-    pluginProgress->setComment("Found Fabric");
-    pluginProgress->progress(1, STEPS);
-  }
-
-  /**
-   * Open file to read and import per type
-   */
   
-  /*std::string filename;
-  
-  dataSet->get("file::filename", filename);
-  std::ifstream ifs(filename.c_str());
-  if(!ifs)
-  {
-    if(pluginProgress)
-      pluginProgress->setError("Unable open source file.");
-
-    return false;
-  }
-
-  if(pluginProgress)
-  {
-    pluginProgress->progress(2, STEPS);
-    pluginProgress->setComment("Parsing Routes.");
-  }
-  
-  ibp::ibdiagnet_fwd_db parser;
-  /*if(!parser.parse(*fabric, ifs))
-  {
-    if(pluginProgress)
-      pluginProgress->setError("Unable parse routes file.");
-
-    return false;
-  }*/
 
   if(pluginProgress)
   {
@@ -136,54 +86,9 @@ bool HelloWorld::run()
     pluginProgress->progress(3, STEPS);
   }
 
-  ifs.close();
+  
       
-  /**
-   * calculate routes outbound
-   * from every port on the fabric
-   */
-  /*tlp::IntegerProperty * ibRoutesOutbound = graph->getProperty<tlp::IntegerProperty >("ibRoutesOutbound");
-  assert(ibRoutesOutbound);
 
-  if(pluginProgress)
-  {
-    pluginProgress->setComment("Calculating Route oversubscription.");
-    pluginProgress->progress(4, STEPS);
-  }
-
-  /*for(
-    ib::fabric_t::entities_t::const_iterator
-    itr = fabric->get_entities().begin(),
-    eitr = fabric->get_entities().end();
-    itr != eitr;
-    ++itr
-  )
-  {
-    const ib::entity_t &entity = itr->second;
-
-    for(
-      ib::entity_t::routes_t::const_iterator
-      ritr =  entity.get_routes().begin(),
-      reitr = entity.get_routes().end();
-      ritr != reitr;
-      ++ritr
-    )
-    {
-      const ib::entity_t::routes_t::mapped_type set = ritr->second;
-      const ib::entity_t::portmap_t::const_iterator port_itr = entity.ports.find(ritr->first);
-      if(port_itr != entity.ports.end())
-      {
-        const ib::port_t* const port = port_itr->second;
-        const ib::tulip_fabric_t::port_edges_t::const_iterator edge_itr = fabric->port_edges.find(const_cast<ib::port_t*>(port));
-        if(edge_itr != fabric->port_edges.end())
-        {
-          const tlp::edge &edge = edge_itr->second;
-        
-          ibRoutesOutbound->setEdgeValue(edge, ritr->second.size());
-        }
-      }
-    }
-  }*/
   tlp::Iterator<node> *itnod = graph->getNodes();
   int v = 0; 
   
